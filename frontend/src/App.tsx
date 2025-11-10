c6e062f05dbdd23f6326388845dfd2bc01989c31
Vinicius Alencar <vsslviniciussousa@gmail.com>
2025-11-10 15:46:12 -0400

    feat(frontend): Conecta com a api do backend e busca tarefas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,77 @@
 import { useState, useEffect } from 'react';
-import './App.css'; 
-import type { Task } from './types/task'; 
+import './App.css';
+import type { Task } from './types/task';
+import api from './services/api';
 
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>([]);
-
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
   useEffect(() => {
     console.log("App montado!");
+    const fetchTasks = async () => {
+      try {
+        setLoading(true);
+        setError(null);
 
-    const mockTasks: Task[] = [
-      { id: "1", title: "Configurar API", status: "Concluídas" },
-      { id: "2", title: "Criar Frontend", status: "Em Progresso" },
-      { id: "3", title: "Estilizar Colunas", status: "A Fazer" },
-    ];
-    setTasks(mockTasks);
+        const response = await api.get<Task[]>('/tasks');
+        setTasks(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError("Falha ao carregar tarefas, verificar o backend.");
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+      fetchTasks();
+    };
+  }, []);
 
-  }, []); 
+
+  if (loading) {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <h1>Meu Kanban</h1>
+        </header>
+        <main>
+          <p>Carregando tarefas...</p>
+        </main>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <h1>Meu Kanban</h1>
+        </header>
+        <main>
+          <p className="error-message">{error}</p>
+        </main>
+      </div>
+    );
+  }
 
   return (
     <div className="app">
       <header className="app-header">
         <h1>Meu Kanban</h1>
       </header>
-      
       <main className="kanban-board">
         {tasks.map(task => (
           <div key={task.id}>
             <p>{task.title} ({task.status})</p>
           </div>
         ))}
+        {tasks.length === 0 && (
+          <p>Nenhuma tarefa encontrada. Que tal adicionar uma?</p>
+        )}
       </main>
     </div>
   );
+
 }
 
 export default App;
